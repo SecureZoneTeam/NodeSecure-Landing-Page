595545764b72961f8a520b31e809ce5dde4b9267
Sebastian Pérez <fer.bellido22@gmail.com>
2026-09-10 18:28:49 -0500

    Revert "Feature/landingpage pricing"

--- js/main.js
+++
@@ -1,9 +0,0 @@
-const navbarToogle = document.getElementById("navbar-toggle");
-const navbarMenu = document.getElementById("navbar-menu");
-
-if(navbarToogle && navbarMenu) {
-    navbarToogle.addEventListener("click", ()=> {
-        const isOpen = navbarMenu.classList.toggle("open");
-        navbarToogle.setAttribute("aria-expanded", isOpen);
-    });
-}
